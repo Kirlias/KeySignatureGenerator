55eb7bd7b689464c6bb824519248b80147b17922
Kirlias <alduinworldeater@gmail.com>
2017-04-05 19:13:31 -0300

    Created lists, and created boolean checks for radio buttons

--- KeySignatureGenerator/KeySignatureGenerator/MainPage.xaml.cs
+++ KeySignatureGenerator/KeySignatureGenerator/MainPage.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Diagnostics;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -26,6 +27,11 @@ namespace KeySignatureGenerator
         public bool random = false;
         public bool major = false;
         public bool minor = false;
+
+        public List<string> majors = new List<string>();
+        public List<string> minors = new List<string>();
+        public List<string> randoms = new List<string>();
+
         public MainPage()
         {
             this.InitializeComponent();
@@ -33,9 +39,10 @@ namespace KeySignatureGenerator
 
         private void btnGenerate_Click(object sender, RoutedEventArgs e)
         {
+            CreateLists();
             if(random)
             {
-
+                
             }
             if (minor)
             {
@@ -49,6 +56,7 @@ namespace KeySignatureGenerator
             {
 
             }
+            
         }
 
         private void rbMinor_Checked(object sender, RoutedEventArgs e)
@@ -70,6 +78,46 @@ namespace KeySignatureGenerator
             random = false;
             major = true;
             minor = false;
+        }
+
+        public void CreateLists()
+        {
+            int iminor = 0;
+            int imajor = 0;
+            AddToList("A major", "Ab major","A minor", "A# minor","B minor", 
+                "B major"," major","C major","C# major","C minor","C# minor",
+                "D major","D minor", "D# minor","E minor","E major","Eb major",
+                "F minor", "F# minor","F major", "F# major","G minor", "G# minor",
+                "G major");
+
+            for (int i = 0; i < randoms.Count; i++)
+            {
+
+                if (randoms[i].Contains("major"))
+                {
+                    majors.Add(randoms[i]);
+                    Debug.WriteLine("Added " + majors[imajor] + " To majors");
+                    imajor++;
+                }
+                else
+                {
+                    
+                    minors.Add(randoms[i]);
+                    Debug.WriteLine("Added " + minors[iminor] + " To minors");
+                    iminor++;
+                }
+            }
+
+        }
+
+        public void AddToList(params string[] list)
+        {
+            for(int i = 0; i < list.Length; i++)
+            {
+                randoms.Add(list[i]);
+                Debug.WriteLine(randoms[i]);
+            }
+            
         }
     }
 }
